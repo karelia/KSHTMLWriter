3b528fb01946f0f5fff9604b26f6a54a35320712
Mike Abdullah <me@mikeabdullah.net>
2010-07-23 11:40:26 +0100

    Start of KSCSSWriter.

---
+++ CSS/KSCSSWriter.h
@@ -0,0 +1,21 @@
+//
+//  KSCSSWriter.h
+//  Sandvox
+//
+//  Created by Mike on 23/07/2010.
+//  Copyright 2010 Karelia Software. All rights reserved.
+//
+
+#import "KSForwardingWriter.h"
+
+
+@interface KSCSSWriter : KSForwardingWriter
+{
+  @private
+    id <KSWriter>   _output;
+}
+
+// Writes the string followed enough newlines to carry on writing
+- (void)writeCSSString:(NSString *)cssString;
+
+@end
---
+++ CSS/KSCSSWriter.m
@@ -0,0 +1,21 @@
+//
+//  KSCSSWriter.m
+//  Sandvox
+//
+//  Created by Mike on 23/07/2010.
+//  Copyright 2010 Karelia Software. All rights reserved.
+//
+
+#import "KSCSSWriter.h"
+
+
+@implementation KSCSSWriter
+
+- (void)writeCSSString:(NSString *)cssString;
+{
+    [self writeString:cssString];
+    if (![cssString hasSuffix:@"\n"]) [self writeString:@"\n"];
+    [self writeString:@"\n"];
+}
+
+@end
