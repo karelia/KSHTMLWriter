a9bd737806a9e47facf8493ce00812d2857c7b15
Mike Abdullah <me@mikeabdullah.net>
2010-11-17 18:29:44 +0000

    Shift DOM writing functionality across to KSDOMToHTMLWriter.

--- DOM/KSDOMToHTMLWriter.h
+++ DOM/KSDOMToHTMLWriter.h
@@ -6,19 +6,39 @@
 //  Copyright 2010 Karelia Software. All rights reserved.
 //
 
-#import "KSHTMLWriter+DOM.h"
+#import "KSHTMLWriter.h"
 
 
-@protocol SVDOMToHTMLWriterDelegate;
+@protocol KSDOMToHTMLWriterDelegate;
 @interface KSDOMToHTMLWriter : KSHTMLWriter
 {
   @private
-    id <SVDOMToHTMLWriterDelegate>   _delegate;
+    id <KSDOMToHTMLWriterDelegate>   _delegate;
 }
 
 
+#pragma mark High Level
+- (void)writeDOMElement:(DOMElement *)element;  // like -outerHTML
+- (void)writeInnerOfDOMNode:(DOMNode *)node;    // like -innerHTML
+- (void)writeDOMRange:(DOMRange *)range;
+
+
+#pragma mark Implementation
+- (void)writeInnerOfDOMNode:(DOMNode *)node startAtChild:(DOMNode *)aNode;
+- (void)startElement:(NSString *)elementName withDOMElement:(DOMElement *)element;    // open the tag and write attributes
+- (DOMNode *)endElementWithDOMElement:(DOMElement *)element;    // returns the next sibling to write
+
+
+#pragma mark Pseudo-delegate
+
+// Default implementation returns element. To customise writing, subclass method to do its own writing and return the node to write instead (generally the element's next sibling)
+- (DOMNode *)willWriteDOMElement:(DOMElement *)element;
+
+- (DOMNode *)didWriteDOMText:(DOMText *)text nextNode:(DOMNode *)nextNode;  // for any post-processing
+
+
 #pragma mark Delegate
-@property(nonatomic, assign) id <SVDOMToHTMLWriterDelegate> delegate;
+@property(nonatomic, assign) id <KSDOMToHTMLWriterDelegate> delegate;
 
 @end
 
@@ -26,6 +46,6 @@
 #pragma mark -
 
 
-@protocol SVDOMToHTMLWriterDelegate <NSObject>
-- (DOMNode *)HTMLWriter:(KSHTMLWriter *)writer willWriteDOMElement:(DOMElement *)element;
+@protocol KSDOMToHTMLWriterDelegate <NSObject>
+- (DOMNode *)HTMLWriter:(KSDOMToHTMLWriter *)writer willWriteDOMElement:(DOMElement *)element;
 @end
--- DOM/KSHTMLWriter+DOM.h
+++ DOM/KSHTMLWriter+DOM.h
@@ -30,24 +30,4 @@
 
 @interface KSHTMLWriter (DOM)
 
-#pragma mark High Level
-- (void)writeDOMElement:(DOMElement *)element;  // like -outerHTML
-- (void)writeInnerOfDOMNode:(DOMNode *)node;    // like -innerHTML
-- (void)writeDOMRange:(DOMRange *)range;
-
-
-#pragma mark Implementation
-- (void)writeInnerOfDOMNode:(DOMNode *)node startAtChild:(DOMNode *)aNode;
-- (void)startElement:(NSString *)elementName withDOMElement:(DOMElement *)element;    // open the tag and write attributes
-- (DOMNode *)endElementWithDOMElement:(DOMElement *)element;    // returns the next sibling to write
-
-
-#pragma mark Pseudo-delegate
-
-// Default implementation returns element. To customise writing, subclass method to do its own writing and return the node to write instead (generally the element's next sibling)
-- (DOMNode *)willWriteDOMElement:(DOMElement *)element;
-
-- (DOMNode *)didWriteDOMText:(DOMText *)text nextNode:(DOMNode *)nextNode;  // for any post-processing
-
-
 @end
