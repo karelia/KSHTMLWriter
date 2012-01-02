d2a553fe6a6def94f3859a006cd09da6bda7c347
Dan Wood <dwood@karelia.com>
2012-01-02 13:08:01 -0800

    update copyrights to 2012

--- KSStringXMLEntityEscaping.h
+++ KSStringXMLEntityEscaping.h
@@ -1,7 +1,7 @@
 //
 //  KSStringXMLEntityEscaping.h
 //
-//  Copyright (c) 2010, Mike Abdullah and Karelia Software
+//  Copyright 2010-2012, Mike Abdullah and Karelia Software
 //  All rights reserved.
 //  
 //  Redistribution and use in source and binary forms, with or without
