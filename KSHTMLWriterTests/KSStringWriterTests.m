04ce5b2471ad2d3c72e497436b9b18a8972e902d
Sam Deane <sam@elegantchaos.com>
2012-01-23 11:04:32 +0000

    added some basic unit tests for KSStringWriter and KSXMLWriter

---
+++ KSHTMLWriterTests/KSStringWriterTests.m
@@ -0,0 +1,38 @@
+//
+//  KSHTMLWriterTests.m
+//  KSHTMLWriterTests
+//
+//  Created by Sam Deane on 18/01/2012.
+//  Copyright (c) 2012 Karelia Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+#import "KSStringWriter.h"
+
+@interface KSStringWriterTests : SenTestCase
+
+@end
+
+@implementation KSStringWriterTests
+
+- (void)testInitiallyEmpty
+{
+    KSStringWriter* output = [[KSStringWriter alloc] init];
+    STAssertTrue([[output string] isEqualToString:@""], @"starts off with empty string");
+    [output release];
+}
+
+- (void)testWriting
+{
+    KSStringWriter* output = [[KSStringWriter alloc] init];
+    [output writeString:@"test"];
+    STAssertTrue([[output string] isEqualToString:@"test"], @"string is correct");
+
+    [output writeString:@"test"];
+    STAssertTrue([[output string] isEqualToString:@"testtest"], @"string is correct");
+
+    [output release];
+}
+
+@end
---
+++ KSHTMLWriterTests/KSXMLWriterTests.m
@@ -0,0 +1,49 @@
+//
+//  KSHTMLWriterTests.m
+//  KSHTMLWriterTests
+//
+//  Created by Sam Deane on 18/01/2012.
+//  Copyright (c) 2012 Karelia Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KSXMLWriterTests : SenTestCase
+
+@end
+
+#import "KSXMLWriter.h"
+#import "KSStringWriter.h"
+
+@implementation KSXMLWriterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testSimpleTag
+{
+    KSStringWriter* output = [[KSStringWriter alloc] init];
+    KSXMLWriter* writer = [[KSXMLWriter alloc] initWithOutputWriter:output];
+    [writer startElement:@"foo" attributes:nil];
+    [writer writeCharacters:@"bar"];
+    [writer endElement];
+    
+    NSString* generated = [output string];
+    [output release];
+    [writer release];
+    
+    STAssertTrue([generated isEqualToString:@"<foo>bar</foo>"], @"generated XML matches");
+}
+
+@end
