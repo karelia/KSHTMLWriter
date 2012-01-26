6be7b61e4c7ffe64c7375f8fc480fd2379e43960
Sam Deane <sam@elegantchaos.com>
2012-01-26 15:34:32 +0000

    improved difference reporting

--- KSHTMLWriterWebkitTestShellTests/KSHTMLWriterWebkitTestShellTests.m
+++ KSHTMLWriterWebkitTestShellTests/KSHTMLWriterWebkitTestShellTests.m
@@ -33,11 +33,16 @@
     
     STAssertTrue(length1 == length2, @"string lengths don't match: for %ld (%@) vs %ld (%@)", length1, string1, length2, string2); 
     
-    for (NSUInteger n = 0; n < length1; ++n)
+    NSUInteger safeLength = MIN(length1, length2);
+    for (NSUInteger n = 0; n < safeLength; ++n)
     {
         UniChar c1 = [string1 characterAtIndex:n];
         UniChar c2 = [string2 characterAtIndex:n];
         STAssertTrue(c1 == c2, @"Comparison failed at character %ld (0x%x '%c' vs 0x%x '%c') of '%@'", n, c1, c1, c2, c2, string1);
+        if (c1 != c2)
+        {
+            break; // in theory we could report every character difference, but it could get silly, so we stop after the first failure
+        }
     }
 }
 
