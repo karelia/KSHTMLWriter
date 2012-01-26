3c6812f5537353edc1cfbe2983abcc634d1f9545
Sam Deane <sam@elegantchaos.com>
2012-01-26 15:54:31 +0000

    added a test for empty content

--- KSHTMLWriterTests/KSXMLWriterTests.m
+++ KSHTMLWriterTests/KSXMLWriterTests.m
@@ -29,6 +29,19 @@
     STAssertTrue([generated isEqualToString:@""], @"generated string is empty");
 }
 
+- (void)testEmptyContent
+{
+    KSStringWriter* output = [[KSStringWriter alloc] init];
+    KSXMLWriter* writer = [[KSXMLWriter alloc] initWithOutputWriter:output];
+    [writer writeElement:@"foo" attributes:nil content:nil];
+    
+    NSString* generated = [output string];
+    [output release];
+    [writer release];
+
+    [self assertString:generated matchesString:@"<foo />"];
+}
+
 - (void)testSimpleTag
 {
     KSStringWriter* output = [[KSStringWriter alloc] init];
@@ -41,12 +54,11 @@
     [output release];
     [writer release];
     
-    STAssertTrue([generated isEqualToString:@"<foo>bar</foo>"], @"generated XML matches");
+    [self assertString:generated matchesString:@"<foo>bar</foo>"];
 }
 
 #if TODO // TODO - list of initial things to test
 
-1. -writeElement:content: (nil content for first, simple test)
 2. -pushAttribute: (multiple calls), followed by -writeElement:content:
 3. -writeCharacters: including the special characters of '<' etc.
 4. -writeComment:
