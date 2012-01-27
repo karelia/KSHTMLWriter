71980ee0728cc8de0ad9aa291c0612eb727c0d00
Sam Deane <sam@elegantchaos.com>
2012-01-27 17:26:40 +0000

    added more compound tests

--- Tests/KSHTMLWriterTests/KSHTMLWriterTestCase.m
+++ Tests/KSHTMLWriterTests/KSHTMLWriterTestCase.m
@@ -10,14 +10,14 @@
     NSUInteger length1 = [string1 length];
     NSUInteger length2 = [string2 length];
     
-    STAssertTrue(length1 == length2, @"string lengths don't match: for %ld (%@) vs %ld (%@)", length1, string1, length2, string2); 
+    STAssertTrue(length1 == length2, @"string lengths don't match: was %ld expected %ld \n\nwas:\n%@\n\nexpected:\n%@", length1, length2, string1, string2); 
     
     NSUInteger safeLength = MIN(length1, length2);
     for (NSUInteger n = 0; n < safeLength; ++n)
     {
         UniChar c1 = [string1 characterAtIndex:n];
         UniChar c2 = [string2 characterAtIndex:n];
-        STAssertTrue(c1 == c2, @"Comparison failed at character %ld (0x%x '%c' vs 0x%x '%c') of '%@'", n, c1, c1, c2, c2, string1);
+        STAssertTrue(c1 == c2, @"Comparison failed at character %ld (was 0x%x '%c' expected 0x%x '%c') of:\n\n%@", n, c1, c1, c2, c2, string1);
         if (c1 != c2)
         {
             break; // in theory we could report every character difference, but it could get silly, so we stop after the first failure
