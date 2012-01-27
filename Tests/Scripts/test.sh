be251264417ad972b6903bdfebe5d564de7a35d8
Sam Deane <sam@elegantchaos.com>
2012-01-27 16:05:00 +0000

    updated test scripts to run both test targets

--- Tests/Scripts/test.sh
+++ Tests/Scripts/test.sh
@@ -3,8 +3,9 @@ echo Testing Mac
 base=`dirname $0`
 config="-configuration Debug"
 sdkMac="macosx"
-targetMac="KSHTMLWriterWebkitTestShellTests"
 
-# build the framework
+# build & run the simple tests
+xcodebuild -target "KSHTMLWriterTests" $config -sdk "$sdkMac" clean build | "${base}/ocunit2junit.rb"
 
-xcodebuild -target "$targetMac" $config -sdk "$sdkMac" clean build | "${base}/ocunit2junit.rb"
+# build & run the webkit tests
+xcodebuild -target "KSHTMLWriterWebkitTestShellTests" $config -sdk "$sdkMac" clean build | "${base}/ocunit2junit.rb"
