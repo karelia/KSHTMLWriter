65389de7071299030502a24c08834a25659d2dda
Sam Deane <sam@elegantchaos.com>
2012-02-24 12:32:29 +0000

    got HTML tests working without need for shell application

--- Tests/KSHTMLWriterWebkitTestShellTests/KSHTMLWriterSnippetTests.m
+++ Tests/KSHTMLWriterWebkitTestShellTests/KSHTMLWriterSnippetTests.m
@@ -10,56 +10,74 @@
 #import "KSStringWriter.h"
 #import "KSXMLWriterDOMAdaptor.h"
 
-#import "AppDelegate.h"
-#import "StubWindowController.h"
 #import "KSWriterTestCase.h"
-
 
 #import <SenTestingKit/SenTestingKit.h>
 #import <WebKit/WebKit.h>
 
-@class StubWindowController;
-
 @interface KSHTMLWriterSnippetTests : KSWriterTestCase
 
-@property (strong, nonatomic) StubWindowController* controller;
+@property (assign, nonatomic) BOOL done;
 
 @end
 
 @implementation KSHTMLWriterSnippetTests
 
-@synthesize controller;
+@synthesize done;
+
+- (void)webView:(WebView *)sender didFinishLoadForFrame:(WebFrame *)frame
+{
+    done = YES;
+}    
 
 #pragma mark - Helpers
 
-+ (id)testCaseWithSelector:(SEL)selector url:(NSURL*)url controller:(StubWindowController*)controller
+- (WebView*)webViewWithStubPage
+{
+    WebView* result = nil;
+    NSError* error = nil;
+    NSURL* stubURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"Stub" withExtension:@"html"];
+    NSString* stubHTML = [NSString stringWithContentsOfURL:stubURL encoding:NSUTF8StringEncoding error:&error];
+    if (stubHTML && !error)
+    {
+        result = [[WebView alloc] initWithFrame: NSMakeRect (0,0,640,480)];
+        result.frameLoadDelegate = self;
+        [[result mainFrame] loadRequest:[NSURLRequest requestWithURL:stubURL]];
+        
+        while (self.done == NO)
+        {
+            [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1.0]];
+        }
+   }
+    
+    return [result autorelease];
+}
+
++ (id)testCaseWithSelector:(SEL)selector url:(NSURL*)url
 {
     NSString* name = [[url lastPathComponent] stringByDeletingPathExtension];
     KSHTMLWriterSnippetTests* test = [super testCaseWithSelector:selector param:url name:name];
-    test.controller = controller;
 
     return test;
-}
-- (void)dealloc
-{
-    [controller release];
-    [super dealloc];
 }
 
 - (void)testWritingSnippetsWithWriterClass:(Class)class
 {
     NSURL* snippetURL = self.dynamicTestParameter;
+
+    WebView* view = [self webViewWithStubPage];
+    DOMDocument* document = view.mainFrame.DOMDocument;
+    DOMHTMLElement* element = (DOMHTMLElement*) [document getElementById:@"content"];
     
     NSError* error = nil;
     NSString* snippetHTML = [NSString stringWithContentsOfURL:snippetURL encoding:NSUTF8StringEncoding error:&error];
-    [self.controller injectContent:snippetHTML];
+    
+    [element setInnerHTML:snippetHTML];
     
     KSStringWriter* output = [[KSStringWriter alloc] init];
     KSHTMLWriter* writer = [[class alloc] initWithOutputWriter:output];
     KSXMLWriterDOMAdaptor* adaptor = [[KSXMLWriterDOMAdaptor alloc] initWithXMLWriter:writer];
     
-    DOMDocument* document = self.controller.webview.mainFrame.DOMDocument;
-    DOMElement* element = [document getElementById:@"content"];
     [adaptor writeInnerOfDOMNode:element];
     
     NSString* written = [output string];
@@ -77,14 +95,17 @@
     NSError* error = nil;
     NSString* inputHTML = [NSString stringWithContentsOfURL:[snippetURL URLByAppendingPathComponent:@"input.html"] encoding:NSUTF8StringEncoding error:&error];
     NSString* outputHTML = [NSString stringWithContentsOfURL:[snippetURL URLByAppendingPathComponent:@"output.html"] encoding:NSUTF8StringEncoding error:&error];
-    [self.controller injectContent:inputHTML];
+
+    WebView* view = [self webViewWithStubPage];
+    DOMDocument* document = view.mainFrame.DOMDocument;
+    DOMHTMLElement* element = (DOMHTMLElement*) [document getElementById:@"content"];
+
+    [element setInnerHTML:inputHTML];
     
     KSStringWriter* output = [[KSStringWriter alloc] init];
     KSHTMLWriter* writer = [[class alloc] initWithOutputWriter:output];
     KSXMLWriterDOMAdaptor* adaptor = [[KSXMLWriterDOMAdaptor alloc] initWithXMLWriter:writer options:KSXMLWriterDOMAdaptorPrettyPrint];
     
-    DOMDocument* document = self.controller.webview.mainFrame.DOMDocument;
-    DOMElement* element = [document getElementById:@"content"];
     [adaptor writeInnerOfDOMNode:element];
     
     NSString* written = [output string];
@@ -114,34 +135,23 @@
 
 + (id) defaultTestSuite
 {
-    StubWindowController* controller = [[StubWindowController alloc] init];
-    [controller.window makeKeyAndOrderFront:self];
-    [controller loadStubPage];
-    
-    while (controller.stubLoaded == NO)
-    {
-        [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:1.0]];
-    }
-
     id result = [[[SenTestSuite alloc] initWithName:NSStringFromClass(self)] autorelease];
     
     // simple html tests
-    NSArray* snippets = [[NSBundle mainBundle] URLsForResourcesWithExtension:@"html" subdirectory:@"Snippets/Normal"];
+    NSArray* snippets = [[NSBundle bundleForClass:[self class]] URLsForResourcesWithExtension:@"html" subdirectory:@"Snippets/Normal"];
     for (NSURL* snippetURL in snippets)
     {
-        [result addTest:[self testCaseWithSelector:@selector(testWritingSnippetWithXMLWriter) url:snippetURL controller:controller]];
-        [result addTest:[self testCaseWithSelector:@selector(testWritingSnippetWithHTMLWriter) url:snippetURL controller:controller]];
+        [result addTest:[self testCaseWithSelector:@selector(testWritingSnippetWithXMLWriter) url:snippetURL]];
+        [result addTest:[self testCaseWithSelector:@selector(testWritingSnippetWithHTMLWriter) url:snippetURL]];
     }
 
     // pretty printing tests
     snippets = [[NSBundle mainBundle] URLsForResourcesWithExtension:nil subdirectory:@"Snippets/Pretty"];
     for (NSURL* snippetURL in snippets)
     {
-        [result addTest:[self testCaseWithSelector:@selector(testWritingSnippetWithHTMLWriterPretty) url:snippetURL controller:controller]];
+        [result addTest:[self testCaseWithSelector:@selector(testWritingSnippetWithHTMLWriterPretty) url:snippetURL]];
     }
     
-    [controller release];
-
     return result;
 }
 
