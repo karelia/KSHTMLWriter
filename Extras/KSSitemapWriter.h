81adbf75fa6360bae488f24e61c39fa9b637aeab
Mike Abdullah <me@mikeabdullah.net>
2012-03-10 23:33:56 +0000

    KSSitemapWriter.

---
+++ Extras/KSSitemapWriter.h
@@ -0,0 +1,41 @@
+//
+//  KSSitemapWriter.h
+//  Sandvox
+//
+//  Created by Mike Abdullah on 10/03/2012.
+//  Copyright (c) 2012 Karelia Software. All rights reserved.
+//
+//  An implementation of the Sitemap protocol <http://www.sitemaps.org/> built atop KSXMLWriter
+//
+
+
+#import "KSXMLWriter.h"
+
+
+extern NSString * const KSSitemapChangeMapFrequencyAlways;
+extern NSString * const KSSitemapChangeMapFrequencyHourly;
+extern NSString * const KSSitemapChangeMapFrequencyDaily;
+extern NSString * const KSSitemapChangeMapFrequencyWeekly;
+extern NSString * const KSSitemapChangeMapFrequencyMonthly;
+extern NSString * const KSSitemapChangeMapFrequencyYearly;
+extern NSString * const KSSitemapChangeMapFrequencyNever;
+
+
+@interface KSSitemapWriter : NSObject
+{
+  @private
+    KSXMLWriter *_writer;
+}
+
+- (id)initWithOutputWriter:(id <KSWriter>)output;
+
+// URL is compulsary; all else optional
+- (void)writeURL:(NSURL *)loc           // should be sub-path of the folder containing the sitemap
+modificationDate:(NSDate *)lastMod
+ changeFrequency:(NSString *)changeFreq
+        priority:(NSNumber *)priority;  // between 0 and 1. If nil, search engines assume 0.5
+
+// Call when done writing URLs to properly end the XML document
+- (void)close;
+
+@end
---
+++ Extras/KSSitemapWriter.m
@@ -0,0 +1,68 @@
+//
+//  KSSitemapWriter.m
+//  Sandvox
+//
+//  Created by Mike Abdullah on 10/03/2012.
+//  Copyright (c) 2012 Karelia Software. All rights reserved.
+//
+
+#import "KSSitemapWriter.h"
+
+
+NSString * const KSSitemapChangeMapFrequencyAlways = @"always";
+NSString * const KSSitemapChangeMapFrequencyHourly = @"hourly";
+NSString * const KSSitemapChangeMapFrequencyDaily = @"daily";
+NSString * const KSSitemapChangeMapFrequencyWeekly = @"weekly";
+NSString * const KSSitemapChangeMapFrequencyMonthly = @"monthly";
+NSString * const KSSitemapChangeMapFrequencyYearly = @"yearly";
+NSString * const KSSitemapChangeMapFrequencyNever = @"never";
+
+
+@implementation KSSitemapWriter
+
+- (id)initWithOutputWriter:(id <KSWriter>)output;
+{
+    if (self = [self init])
+    {
+        _writer = [[KSXMLWriter alloc] initWithOutputWriter:output encoding:NSUTF8StringEncoding];
+        [_writer writeString:@"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"];
+        
+        [_writer pushAttribute:@"xmlns" value:@"http://www.sitemaps.org/schemas/sitemap/0.9"];
+        [_writer startElement:@"urlset"];
+    }
+    
+    return self;
+}
+
+- (void)writeURL:(NSURL *)loc modificationDate:(NSDate *)lastMod changeFrequency:(NSString *)changeFreq priority:(NSNumber *)priority;
+{
+    [_writer writeElement:@"url" content:^{
+        [_writer writeElement:@"loc" text:[loc absoluteString]];
+        
+        if (lastMod)
+        {
+            NSString *lastModText = [lastMod descriptionWithCalendarFormat:@"%Y-%m-%dT%H:%M:%SZ"
+                                                                  timeZone:[NSTimeZone timeZoneWithAbbreviation:@"GMT"]
+                                                                    locale:nil];
+            
+            [_writer writeElement:@"lastmod" text:lastModText];
+        }
+        
+        if (changeFreq) [_writer writeElement:@"changefreq" text:changeFreq];
+        if (priority) [_writer writeElement:@"priority" text:[NSString stringWithFormat:@"%.02f", [priority floatValue]]];
+    }];
+}
+
+- (void)close;
+{
+    [_writer endElement];   // </urlset>
+    [_writer close];
+    [_writer release]; _writer = nil;
+}
+
+- (void)dealloc
+{
+    [self close];   // releases _writer
+}
+
+@end
