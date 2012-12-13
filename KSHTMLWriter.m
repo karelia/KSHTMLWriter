5d1fcbe5d5629978d2b654da430524beabdf7fcd
Mike Abdullah <me@mikeabdullah.net>
2012-12-13 18:56:04 +0000

    typo

--- KSHTMLWriter.m
+++ KSHTMLWriter.m
@@ -353,7 +353,7 @@ NSString *KSHTMLWriterDocTypeHTML_5 = @"html";
     else
     {
         // Embedded scripts should start on their own line for clarity
-        // Outdent the script comapred to wha'ts normal
+        // Outdent the script comapred to what's normal
         [self startElement:@"script" writeInline:NO];
         
         if (!src)
