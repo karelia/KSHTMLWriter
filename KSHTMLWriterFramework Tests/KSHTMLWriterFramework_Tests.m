8ee07c968e388acb99f9a26c7be16e459e696cd5
Mike Abdullah <me@mikeabdullah.net>
2015-05-12 17:10:06 +0200

    Spin up a new bundle for testing

---
+++ KSHTMLWriterFramework Tests/KSHTMLWriterFramework_Tests.m
@@ -0,0 +1,40 @@
+//
+//  KSHTMLWriterFramework_Tests.m
+//  KSHTMLWriterFramework Tests
+//
+//  Created by Mike on 12/05/2015.
+//  Copyright (c) 2015 Karelia Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface KSHTMLWriterFramework_Tests : XCTestCase
+
+@end
+
+@implementation KSHTMLWriterFramework_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
